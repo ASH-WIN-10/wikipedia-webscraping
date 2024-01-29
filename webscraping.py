13524e176477dae88977776f27fd4cd4743a6619
Ashvin <ashvinjangid10@gmmail.com>
2024-01-29 21:22:16 +0530

    Modified the code a bit for better user experience and made it a little faster.

--- webscraping.py
+++ webscraping.py
@@ -2,12 +2,6 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 def search_and_write(user_Input):
-    if userInput == "exit()":
-        return "Thank you!!"
-    
-    #opening the file
-    out = open('output.txt', 'a', encoding='utf-8')
-
     page = urllib.request.urlopen('https://en.wikipedia.org/wiki/' + user_Input)
     content = page.read().decode("utf-8")
 
@@ -16,13 +10,19 @@ def search_and_write(user_Input):
     paragraph = content[start_index:last_index]
 
     final_output = BeautifulSoup(paragraph, "html.parser")
-    out.write(final_output.get_text())
-    out.write("\n\n")
-    #closing the file
-    out.close()
 
-userInput = '1'
-print("Enter 0 to exit!!")
-while userInput!='exit()':
+    with open("output.txt", "a", encoding="utf-8") as output_file:
+        output_file.write(final_output.get_text())
+        output_file.write("\n\n")
+    print("Done!!\n")
+
+userInput = None
+print("Enter 'exit()' to exit!!")
+while userInput != "exit()":
     userInput = input("What do you want to search about? ")
+    if userInput == "exit()":
+        print("Thank you!!")
+        break
+    else:
+        print("Your output is being printed in the file. Please wait...")
     search_and_write(userInput)
